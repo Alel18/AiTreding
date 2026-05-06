ec68a68f68f8a40524e708c4066960718649da4a
Alel18 <saatalbabi25@gmail.com>
2026-05-06 16:58:20 +0700

    Add Telegram notification functionality

--- bot_trading.py
+++ bot_trading.py
@@ -1,1 +1,19 @@
-print("AI Trading Jalan")
+import requests
+
+TOKEN = "8775385140:AAG6Mt-_4r7Mq7s1RYjWRkMqYpn_EUiB7E4"
+CHAT_ID = "6809245174"
+
+def kirim_telegram(pesan):
+
+    url = f"https://api.telegram.org/bot{TOKEN}/sendMessage"
+
+    params = {
+        "chat_id": CHAT_ID,
+        "text": pesan
+    }
+
+    r = requests.get(url, params=params)
+
+    print(r.text)
+
+kirim_telegram("AI Trading GitHub Berhasil Jalan")
